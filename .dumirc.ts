e469fc80fadee8eb59a141604d8ab58657965a90
liyang0612 <356514033@qq.com>
2023-04-30 11:47:30 +0800

    fix: update .dumirc

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,8 @@ const path = require('path');
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '.',
+  publicPath: '/components-in-mind/',
   alias: {
     'components-in-mind': path.resolve(__dirname, '/src')
   },
